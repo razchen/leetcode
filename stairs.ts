59450572d22074d634f4ae87d10be0e1a1f6bcb6
Raz <gptraz@gmail.com>
2025-05-21 17:06:58 -0600

    climbing stairs and env

--- stairs.ts
+++ stairs.ts
@@ -1,5 +1,7 @@
 function climbStairs(n: number): number {
-  const dfs = (i) => {
+  const memo: number[] = [];
+
+  const dfs = (i: number): number => {
     if (i === n) {
       return 1;
     }
@@ -8,10 +10,48 @@ function climbStairs(n: number): number {
       return 0;
     }
 
-    return dfs(i + 1) + dfs(i + 2);
+    if (memo[i]) {
+      return memo[i];
+    } else {
+      memo[i] = dfs(i + 1) + dfs(i + 2);
+    }
+
+    return memo[i];
   };
 
   return dfs(0);
 }
 
-climbStairs(3);
+function climbStairsBottomUp(n: number): number {
+  const bottomUp: number[] = [];
+
+  const dfs = (i: number): number => {
+    if (i === n) {
+      console.log("here");
+      return 1;
+    }
+
+    if (i > n) {
+      return 0;
+    }
+
+    if (bottomUp[i]) {
+      console.log("here");
+      return bottomUp[i];
+    } else {
+      bottomUp[0] = 1;
+      bottomUp[1] = 1;
+      for (let i = 2; i < n + 1; i++) {
+        bottomUp[i] = bottomUp[i - 1] + bottomUp[i - 2];
+      }
+
+      console.log(bottomUp);
+      return bottomUp[n];
+    }
+  };
+
+  return dfs(0);
+}
+
+console.log(climbStairs(6));
+// console.log(climbStairsBottomUp(6));
